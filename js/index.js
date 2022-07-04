ee08a74224a389b10e5c488bccd79c80cca533f3
igorchagov <igorchagov@mail.ru>
2022-07-04 17:15:11 +0300

    new code js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,16 @@
 let character = document.querySelectorAll('.character')
 let otherWord = document.querySelectorAll('.other-word')
-window.addEventListener('scroll', () =>{
-  if (window.pageYOffset > 100){
-  character[2].classList.add('character-active')
-  otherWord[2].classList.add('other-word__active')
- }
- if (window.pageYOffset > 400) {  
-  character[3].classList.add('character-active')
-  otherWord[3].classList.add('other-word__active')
- }
-})
-
-
-//функция видимости элемента
-var block_show = null;
-
+console.log(character[1].getBoundingClientRect().top)
 
 function scrollTracking() {
- var wt = $(window).scrollTop();
  var wh = $(window).height();
- var et = $(character[0]).offset().top;
- var eh = $(character[0]).outerHeight();
-
- if (wt + wh >= et && wt + wh - eh * 2 <= et + (wh - eh)) {
-  if (block_show == null || block_show == false) {
-   console.log('Блок active в области видимости');
-   character[0].classList.add('character-active')
-   character[1].classList.add('character-active')
-   otherWord[0].classList.add('other-word__active')
-   otherWord[1].classList.add('other-word__active')
+  for (let i = 0; i < character.length; i++) {
+    if (character[i].getBoundingClientRect().top + 300 < wh) {
+      character[i].classList.add('character-active')
+      otherWord[i].classList.add('other-word__active')
+    }
   }
-  block_show = true;
- } else {
-  if (block_show == null || block_show == true) {
-   console.log('Блок active скрыт');
-  }
-  block_show = false;
  }
-}
 
 
 $(window).scroll(function () {
