db892a5f67932db9627cead3dc5a7f7cfff3af72
igorchagov <igorchagov@mail.ru>
2022-07-04 22:38:50 +0300

    change

--- js/index.js
+++ js/index.js
@@ -5,10 +5,19 @@ console.log(character[1].getBoundingClientRect().top)
 function scrollTracking() {
  var wh = $(window).height();
   for (let i = 0; i < character.length; i++) {
-    if (character[i].getBoundingClientRect().top + 300 < wh) {
+   if (wh > 900){ 
+    if (character[i].getBoundingClientRect().top + 300 < wh ) {
+      console.log(wh)
       character[i].classList.add('character-active')
       otherWord[i].classList.add('other-word__active')
     }
+  }else if (wh < 900) {
+     if (character[i].getBoundingClientRect().top - 200< wh) {
+       console.log(character[3].getBoundingClientRect().top)
+       character[i].classList.add('character-active')
+       otherWord[i].classList.add('other-word__active')
+     }
+  }
   }
  }
 
